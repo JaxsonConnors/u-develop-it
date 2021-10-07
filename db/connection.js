76a7ce3944454f5ffbe58bc531d852eb0d2f85c6
Jaxson Connors <connorsjaxson@gmail.com>
2021-10-07 16:51:50 -0600

    added code snapshot

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,12 @@
-const mysql = require("mysql2");
-// Connect to database
+const mysql = require('mysql2');
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    // Your MySQL username,
-    user: 'root',
-    // Your MySQL password
-    password: 'MyNewPassword1',
-    database: 'election'
-  });
-  module.exports = db;
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: '',
+  database: 'election'
+});
+
+module.exports = db;
